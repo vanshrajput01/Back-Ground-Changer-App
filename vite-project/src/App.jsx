8aa6bbe60b5d76bb21cd0d9354ee771d09a6d9be
vanshrajput01 <rajputvansh009@gmail.com>
2023-11-04 12:24:20 +0530

    Second commit for App Css and other functions are update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react"
+import './App.css'
+
 function App() {
+  const [color ,setcolor ] = useState("while")
+
 
 
   return (
-    <>
-    
+    <div style={
+      {
+        width : "100vw",
+        height : "100vh", 
+        backgroundColor: color,
+        display : "flex",
+        justifyContent: "center",
+        alignItems :"e"
+
+      }
+      }>
+      <footer className="footer-container">
+        <button className = "btn btn-r" onClick={()=>{setcolor("grey")}}>Grey</button>
+        <button className = "btn btn-y" onClick={()=>{setcolor("antiquewhite")}}>Antiquewhite</button>
+        <button className = "btn btn-g" onClick={()=>{setcolor("green")}}>Green</button>
+        <button className = "btn btn-a" onClick={()=>{setcolor("aqua")}}>Aqua</button>
+        <button className = "btn btn-b" onClick={()=>{setcolor("blue")}}>Blue</button>
+        <button className = "btn btn-o" onClick={()=>{setcolor("orange")}}>Orange</button>
+
+
+      </footer>
+
     
 
-    </>
+    
+
+    </div>
   )
 }
 
